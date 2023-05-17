f6e68162b1725b19eb31e5e281397f896c53d616
Orsi Suto <orsolya.suto.so@gmail.com>
2023-05-17 16:38:57 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import 'react-mdl/extra/material.js';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  , document.getElementById('root'));
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<App />
+	</BrowserRouter>,
+	document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
